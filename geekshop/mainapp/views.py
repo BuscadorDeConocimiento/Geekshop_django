a4d608916179f0a48bd0cc7ce8e998acb02fb0ed
BuscadorDeConocimiento <fwm5040@mail.ru>
2019-08-26 11:38:05 +0300

    django lesson 2

--- geekshop/mainapp/views.py
+++ geekshop/mainapp/views.py
@@ -3,11 +3,29 @@ from django.shortcuts import render
 # Create your views here.
 
 def main(request):
-    return render(request, 'index.html')
+    context = {
+        'title': 'Главная'
+    }
+    return render(request, 'index.html', context=context)
 
 def products(request):
-    return render(request, 'products.html')
+    links_menu = [
+        {'href': 'products_all', 'name': 'все'},
+        {'href': 'products_home', 'name': 'дом'},
+        {'href': 'products_office', 'name': 'офис'},
+        {'href': 'products_modern', 'name': 'модерн'},
+        {'href': 'products_classic', 'name': 'классик'},
+    ]
+    context = {
+        'title': 'Продукты',
+        'links_menu': links_menu,
+        #'same_products': same_products,
+    }
+    return render(request, 'products.html', context=context)
 
 def contact(request):
-    return render(request, 'contact.html')
+    context = {
+        'title': 'Контакты'
+    }
+    return render(request, 'contact.html', context=context)
 
